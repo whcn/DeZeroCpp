d5d65f2ee2f384b4d338cd03dd9c9ef55ac34d3c
Wu Huan <huan.wu@momenta.ai>
2025-08-04 20:21:52 +0800

    add [arm64] googletest for unit tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
-#include <Eigen/Dense>
 
 
 int main() {
-    // 创建2x3随机矩阵（类似np.random.rand(2,3)）
-    Eigen::MatrixXd mat = Eigen::MatrixXd::Random(2, 3);
-    std::cout << "随机矩阵:\n" << mat << "\n\n";
-
-    // 创建3维向量并初始化（类似np.array([1,2,3])）
-    Eigen::Vector3d vec(1.0, 2.0, 3.0);
-    std::cout << "向量:\n" << vec << std::endl;
     return 0;
 }
